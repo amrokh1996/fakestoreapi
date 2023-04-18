331a1e7a20446a3cd8b3c4bfc5add5a462d31cb6
amrokh1996 <amrrrkh1996@gmail.com>
2023-04-18 12:31:11 +0300

    use class to save proguct data

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
 const div = document.getElementById("product");
 let productArray=[];
 
+productData()
 
 
 
-fetch('https://fakestoreapi.com/products')
+async function productData(){
+await fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => {
-    data.map(data =>{
-      div.innerHTML+= `
-      <div class="thumb-wrapper">
-      <span class="wish-icon"><i class="fa fa-heart-o"></i></span>
-      <div class="img-box">
-          <img src="${data.image}" class="img-fluid" alt="">									
-      </div>
-      <div class="thumb-content">
-          <h4>${data.category}</h4>									
-          <div class="star-rating">
-              <ul class="list-inline">
-                  <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                  <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                  <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                  <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                  <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
-              </ul>
-          </div>
-          <p class="item-price"><strike>$${data.price}</strike> <b>$${data.price-Math.floor((data.price)*0.1)}</b></p>
-          <a href="#" class="btn btn-primary">Add to Cart</a>
-      </div>						
-  </div>`
-    })
-
+    for (let i = 0; i < data.length; i++) {
+      let product = new Product(data[i]);
+      productArray.push(product);
+    }
+    
   })
   .catch(error => {
     console.error(error);
   });
+
+
+  productArray.map(product =>{
+    div.innerHTML+= `
+    <div class="thumb-wrapper">
+    <span class="wish-icon"><i class="fa fa-heart-o"></i></span>
+    <div class="img-box">
+        <img src="${product.image}" class="img-fluid" alt="">									
+    </div>
+    <div class="thumb-content">
+        <h4>${product.category}</h4>									
+        <div class="star-rating">
+            <ul class="list-inline">
+                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+            </ul>
+        </div>
+        <p class="item-price"><strike>$${product.price}</strike> <b>$${product.price-Math.floor((product.price)*0.1)}</b></p>
+        <a href="#" class="btn btn-primary">Add to Cart</a>
+    </div>						
+</div>`
+  })
+
+}
+
+class Product{
+  constructor(data){
+    this.category = data.category,
+    this.image = data.image,
+    this.price = data.price,
+    this.description = data.description
+  }
+}
+
+
+
+
 
   
 
